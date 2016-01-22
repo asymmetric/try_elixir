04e98966f5955e799e88074a033acfcd0a75b970
Lorenzo Manacorda <lorenzo@mailbox.org>
2016-01-22 16:01:17 +0100

    use jquery to trigger autoindent

--- web/static/js/cm-init.js
+++ web/static/js/cm-init.js
@@ -1,6 +1,8 @@
-let div = document.getElementById("cdm");
+let div = document.getElementById("editor");
 let cm = CodeMirror(div, {
   lineNumbers: true,
   theme: "elegant",
   mode: "elixir"
 });
+
+$("#indent").click(() => cm.execCommand("indentAuto"));
