f87748a87ba98f8f907b9c805aa806c3da8cd9ed
Lorenzo Manacorda <lorenzo@mailbox.org>
2016-01-22 16:02:17 +0100

    autofocus

--- web/static/js/cm-init.js
+++ web/static/js/cm-init.js
@@ -2,7 +2,8 @@ let div = document.getElementById("editor");
 let cm = CodeMirror(div, {
   lineNumbers: true,
   theme: "elegant",
-  mode: "elixir"
+  mode: "elixir",
+  autofocus: true
 });
 
 $("#indent").click(() => cm.execCommand("indentAuto"));
