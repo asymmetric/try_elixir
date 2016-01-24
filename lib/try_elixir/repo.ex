f885119b2599fc9214387280729551c17552cebb
Lorenzo Manacorda <lorenzo@mailbox.org>
2016-01-24 18:35:02 +0100

    remove the db

--- lib/try_elixir/repo.ex
+++
@@ -1,3 +0,0 @@
-defmodule TryElixir.Repo do
-  use Ecto.Repo, otp_app: :try_elixir
-end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,6 +1,1 @@
 ExUnit.start
-
-Mix.Task.run "ecto.create", ~w(-r TryElixir.Repo --quiet)
-Mix.Task.run "ecto.migrate", ~w(-r TryElixir.Repo --quiet)
-Ecto.Adapters.SQL.begin_test_transaction(TryElixir.Repo)
-
