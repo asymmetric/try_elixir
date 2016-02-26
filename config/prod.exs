5ac4e4a5de3655f180d7cf74b7c0677d31c42301
Lorenzo Manacorda <lorenzo@mailbox.org>
2016-02-26 15:40:09 +0100

    revert back to HTTP

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :try_elixir, TryElixir.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "try-elixir-phoenix.herokuapp.com", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  url: [scheme: "http", host: "try-elixir-phoenix.herokuapp.com", port: 80],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
