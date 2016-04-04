684632b384517e21ae72c7ada98b1ee6619b7dc0
Lorenzo Manacorda <lorenzo@mailbox.org>
2016-04-04 15:25:31 +0200

    additional :init functions added to blacklist

--- web/controllers/command_controller.ex
+++ web/controllers/command_controller.ex
@@ -10,20 +10,15 @@ defmodule TryElixir.CommandController do
   defp safe_input?(content) do
     cond do
       String.match? content, ~r/:erlang.halt/ -> {:unsafe, content}
-      String.match? content, ~r/:init.stop/ -> {:unsafe, content}
+      String.match? content, ~r/:init.(stop|restart|reboot|boot)/ -> {:unsafe, content}
       true -> {:safe, content}
     end
   end
 
-  defp safe_render({:unsafe, _content}, conn) do
-    conn
-    |> json(%{resp: "no no no"})
-  end
-
+  defp safe_render({:unsafe, _content}, conn), do: conn |> json(%{resp: "no no no"})
   defp safe_render({:safe, content}, conn) do
     result = TryElixir.CodeEvaluator.run(content)
 
-    conn
-    |> json(%{resp: result})
+    conn |> json(%{resp: result})
   end
 end
