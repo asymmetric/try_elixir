5c480ce0cffa44cbfd09e71b3e0685f70d2cef5b
Lorenzo Manacorda <lorenzo@mailbox.org>
2016-04-04 13:26:05 +0200

    clean-up input checking

--- web/controllers/command_controller.ex
+++ web/controllers/command_controller.ex
@@ -2,21 +2,28 @@ defmodule TryElixir.CommandController do
   use TryElixir.Web, :controller
 
   def create(conn, %{"content" => content}) do
-    case safe_input?(content) do
-      :unsafe ->
-        conn |> json(%{resp: "no no no"})
-      :safe ->
-        result = TryElixir.CodeEvaluator.run(content)
-
-        conn |> json(%{resp: result})
-    end
+    content
+    |> safe_input?
+    |> safe_render(conn)
   end
 
   defp safe_input?(content) do
     cond do
-      String.match? content, ~r/:erlang.halt/ -> :unsafe
-      String.match? content, ~r/:init.stop/ -> :unsafe
-      true -> :safe
+      String.match? content, ~r/:erlang.halt/ -> {:unsafe, content}
+      String.match? content, ~r/:init.stop/ -> {:unsafe, content}
+      true -> {:safe, content}
     end
+  end
+
+  defp safe_render({:unsafe, _content}, conn) do
+    conn
+    |> json(%{resp: "no no no"})
+  end
+
+  defp safe_render({:safe, content}, conn) do
+    result = TryElixir.CodeEvaluator.run(content)
+
+    conn
+    |> json(%{resp: result})
   end
 end
